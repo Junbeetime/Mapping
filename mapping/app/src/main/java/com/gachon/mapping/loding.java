01f861186b96cc28182d572a5cc91556df824046
Junbeetime <95276922+Junbeetime@users.noreply.github.com>
2022-05-09 13:31:14 +0900

    loding

---
+++ mapping/app/src/main/java/com/gachon/mapping/loding.java
@@ -0,0 +1,14 @@
+package com.gachon.mapping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loding);
+    }
+}
