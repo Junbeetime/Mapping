ac6bed9f36575a54d354cba8da137a03584c6c10
jhg83422 <jhg8342@gachon.ac.kr>
2022-05-28 20:39:47 +0900

    5-28-20:40

--- app/src/main/java/com/gachon/mapping/LoginActivity.java
+++ app/src/main/java/com/gachon/mapping/LoginActivity.java
@@ -6,13 +6,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    boolean flag;
+    Button loginbutton;
+    Button signupbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        flag = true;
+        InitalizeView();
+        LoginListener();
+        SignupListener();
     }
+
+    public void InitalizeView(){
+        loginbutton = (Button) findViewById(R.id.login_login_Button);
+        signupbutton = (Button) findViewById(R.id.login_signup_Button);
+    }
+
+    public void LoginListener(){
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag == true){
+                Intent startintent =new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startintent);}
+                else{
+                    Toast.makeText(getApplicationContext(),"로그인 정보가 옳바르지 않습니다",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void SignupListener(){
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupintent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(signupintent);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/gachon/mapping/StartActivity.java
+++ app/src/main/java/com/gachon/mapping/StartActivity.java
@@ -9,13 +9,31 @@ import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button startbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        this.InitalizeView();
+        StartListener();
 
     }
 
+    public void InitalizeView(){
+        startbutton =(Button) findViewById(R.id.text_start_button);
+    }
+
+    public void StartListener(){
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startintent =new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(startintent);
+            }
+        });
+    }
 
 }
