dac416cbe2ce583abb761d58dd3d33b0d8da5e36
jhg83422 <jhg8342@gachon.ac.kr>
2022-05-28 23:38:58 +0900

    5-28-21:14

--- app/src/main/java/com/gachon/mapping/Diary.java
+++ app/src/main/java/com/gachon/mapping/Diary.java
@@ -3,12 +3,41 @@ package com.gachon.mapping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Diary extends AppCompatActivity {
+public class Diary extends AppCompatActivity implements View.OnClickListener {
+
+    // 변수선언
+    Button savebutton;
+
+    EditText subjecttext;
+    EditText contenttext;
+
+    // DBHelper 선언
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        InitalizeView();
+
+
+
+    }
+
+    public void InitalizeView(){
+        savebutton = (Button) findViewById(R.id.diary_save_button);
+        subjecttext = (EditText) findViewById(R.id.diary_subject);
+        contenttext = (EditText) findViewById(R.id.diary_content);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
---
+++ app/src/main/java/com/gachon/mapping/Tableinfo.java
@@ -0,0 +1,10 @@
+package com.gachon.mapping;
+
+public class Tableinfo {
+    public static final String TABLE_NAME = "diary_info";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_IMAGE = "image";
+    public static final String COLUMN_CONTENT = "content";
+
+}
