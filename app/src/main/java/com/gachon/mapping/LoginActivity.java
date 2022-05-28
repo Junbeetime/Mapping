37c36f1c8c95cafce8eebfae41f3f19d6d6a134f
jhg83422 <jhg8342@gachon.ac.kr>
2022-05-29 02:14:54 +0900

    5-29-02:14

--- app/src/main/java/com/gachon/mapping/LoginActivity.java
+++ app/src/main/java/com/gachon/mapping/LoginActivity.java
@@ -1,19 +1,29 @@
 package com.gachon.mapping;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     boolean flag;
-    Button loginbutton;
-    Button signupbutton;
-    Button findbutton;
+    private Button loginbutton;
+    private Button signupbutton;
+    private Button findbutton;
+    private EditText email_text;
+    private EditText pwd_text;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +31,40 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         flag = true;
         InitalizeView();
-        LoginListener();
         SignupListener();
         FindListener();
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = email_text.getText().toString().trim();
+                String pwd = pwd_text.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(id,pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Toast.makeText(LoginActivity.this,"로그인 실패",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 
     public void InitalizeView(){
         loginbutton = (Button) findViewById(R.id.login_login_Button);
         signupbutton = (Button) findViewById(R.id.login_signup_Button);
         findbutton = (Button) findViewById(R.id.login_find_Button);
+        email_text = (EditText) findViewById(R.id.login_textid);
+        pwd_text = (EditText) findViewById(R.id.login_text_password);
     }
 
     public void LoginListener(){
--- app/src/main/java/com/gachon/mapping/SignUpActivity.java
+++ app/src/main/java/com/gachon/mapping/SignUpActivity.java
@@ -1,14 +1,77 @@
 package com.gachon.mapping;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText sign_email_text;
+    private EditText sign_pwd_text;
+    private EditText sign_age_text;
+    private EditText sign_phone_text;
+    private Button sign_button;
+
+    FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        InitalizeView();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        sign_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = sign_email_text.getText().toString().trim();
+                String pwd = sign_pwd_text.getText().toString().trim();
+                String age = sign_age_text.getText().toString().trim();
+                String phone = sign_phone_text.getText().toString().trim();
+                // 공백 부분 제거 --> trim
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>(){
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+
     }
+
+    public void InitalizeView(){
+        sign_email_text = (EditText) findViewById(R.id.sign_email_edittext);
+        sign_pwd_text = (EditText) findViewById(R.id.sign_password_edittext);
+        sign_age_text = (EditText) findViewById(R.id.sign_age_edittext);
+        sign_phone_text = (EditText) findViewById(R.id.sign_phone_edittext);
+        sign_button = (Button) findViewById(R.id.sign_next_button);
+    }
+
 }
